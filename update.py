4ad9f7574031b6252e0aeec6c8a0171cc0c94af6
djrusso <dan.joseph.russo@gmail.com>
2020-05-19 11:41:27 -0400

    Update May 2020

--- update.py
+++ update.py
@@ -1,8 +1,8 @@
 # This is my python script to update my website with jemdoc
-
+import sys
 import os
-
-files = ['index', 'biography', 'research']
+print sys.version
+files = ['index', 'research', 'teaching']
 
 for i in files:
-	os.system('python jemdoc.py '+i)
+	os.system('C:\Python27\python.exe jemdoc.py '+i)
